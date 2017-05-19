fa39a56607adf257b6ec12f7e08780b1891472ab
Eduarda Helena <dudinhahelenaa@gmail.com>
2017-05-19 10:00:02 -0300

    Caminho Unico Acendendo

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -3,25 +3,32 @@ using UnityEngine;
 
 public static class PathFinder
 {
-
 	public static List<Grid.Position> FindPath (Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
 	{
 		var path = new List<Grid.Position>();
 		path.Add( fromPosition );
 		path.Add( toPosition );
 
-		while (fromPosition.x != toPosition.x) {
-			fromPosition.x++;
-			path.Add (fromPosition);
+		while (fromPosition.x != toPosition.x || fromPosition.y != toPosition.y) {
 
-
-
+			if (fromPosition.y != toPosition.y) {
+				if (fromPosition.y < toPosition.y)
+					fromPosition.y++;
+				else
+					fromPosition.y--;
+			}
+			if (fromPosition.x != toPosition.x) {
+				if (fromPosition.x < toPosition.x)
+					fromPosition.x++;
+				else
+					fromPosition.x--;
+			}
+			path.Add(fromPosition);
+			if (fromPosition.x == toPosition.x && fromPosition.y == toPosition.y) {	
+				break;
+			}
 		}
-
-
-			
-	
-
 		return path;
+		path.Clear();
 	}
 }
